af8ca002f7d666f89f86e421305f0dd353e828ca
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2024-08-31 01:23:01 +0600

    add Schedule run time daily

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('account_expiration:alert');
+Schedule::command('account_expiration:alert')->dailyAt('02:00'); // daily at 2 AM
+Schedule::command('accounts:update-expired')->dailyAt('02:00'); // daily at 2 AM
